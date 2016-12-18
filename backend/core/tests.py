62d657ee60d07b5104a2370b0d11a121f0a0ca8f
Sardor Muminov <smuminov@gmail.com>
2016-12-18 21:24:57 +0900

    Unit test `core.Service`

--- backend/core/tests.py
+++ backend/core/tests.py
@@ -1,3 +1,26 @@
+# Django
 from django.test import TestCase
 
-# Create your tests here.
+# Core
+from core.models import Service
+
+
+class ServiceTestCase(TestCase):
+
+    def setUp(self):
+        self.service_512mb = Service.objects.create(
+            name='test service 512mb',
+            price=0,
+            capacity=536870912)
+        self.service_1gb = Service.objects.create(
+            name='test service 1gb',
+            price=0,
+            capacity=1073741824)
+
+    def tearDown(self):
+        del self.service_512mb
+        del self.service_1gb
+
+    def test_service_capacity(self):
+        self.assertEqual(self.service_512mb.capacity, 536870912)
+        self.assertEqual(self.service_1gb.capacity, 1073741824)
